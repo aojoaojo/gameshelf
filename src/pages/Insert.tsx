82b54d3c7b159de41e05ba0c4ef98fc5a55eab36
aojoaojo <aojoaojov@gmail.com>
2024-11-03 01:12:12 -0300

    Adicionando link gpt e ajustes visuais

--- src/pages/Insert.tsx
+++ src/pages/Insert.tsx
@@ -58,33 +58,31 @@ export function Insert() {
     }
 
     return (
-        <div>
+        <div className="container-insert">
             <Topo />
-            <div className="bg-light fundo insert-div">
-
+            <div className="bg-light insert-div">
+                <h4 className="text-center"><a className="link-underline link-underline-opacity-0 link-ia" href="https://chatgpt.com/share/6726f0b8-d0a0-800f-b661-3a7778ffffec" target="_blank">Ir ao chatgpt para formatar os dados!</a></h4>
                 <textarea
-                    className="w-50 m-1"
+                    className="w-100 m-1 insert-textarea"
                     name="game"
                     id="game"
-                    cols={50}
-                    rows={20}
                     placeholder={`[
-                    {
-                        "nome": "Catan",
-                        "num_jogadores_min": 3,
-                        "num_jogadores_max": 4,
-                        "tempo_medio_partida": 60,
-                        "data_lancamento": "1995",
-                        "fabricante": "Kosmos",
-                        "designer": "Klaus Teuber",
-                        "categoria": "Estratégia",
-                        "idade_minima": 10,
-                        "dificuldade": "Médio",
-                        "classificacao": 4.5,
-                        "expansoes": null
-                        }
+    {
+        "nome": "Catan",
+        "num_jogadores_min": 3,
+        "num_jogadores_max": 4,
+        "tempo_medio_partida": 60,
+        "data_lancamento": "1995",
+        "fabricante": "Kosmos",
+        "designer": "Klaus Teuber",
+        "categoria": "Estratégia",
+        "idade_minima": 10,
+        "dificuldade": "Médio",
+        "classificacao": 4.5,
+        "expansoes": null
+        }
 ]`                  }></textarea>
-                <button className="w-50" onClick={insertGame}>Inserir</button>
+                <button className="btn m-3 bt-home" onClick={insertGame}>Inserir</button>
             </div>
         </div >
     );
